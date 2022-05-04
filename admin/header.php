0ce5435ef19806a134a888cc0a5768d74791f784
yoelagustinus <yoelagustinus7@gmail.com>
2022-05-04 13:10:01 +0700

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -16,28 +16,43 @@
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-        <title>Admin|March Campaign</title>
+        <title>Admin Project Panel</title>
     </head>
 
     <body>
 
         <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#994650;">
             <div class="container">
-                <a class="navbar-brand">Admin|March Campaign</a>
+                <a class="navbar-brand">Admin Project Panel</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link  <?php if($page=='pending_comment'){echo 'active';}?>" href="PendingComment.php">Pending Comments</a>
+                    
+                    <li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Edit Comment
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li>
+                                    <a class="dropdown-item  <?php if($page=='pending_comment'){echo 'active';}?>" href="PendingComment.php">Pending Comments</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item  <?php if($page=='approve_comment'){echo 'active';}?>" href="ApproveComment.php">Approved Comments</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item  <?php if($page=='spam_comment'){echo 'active';}?>" href="SpamComment.php">Spam Comments</a>
+                                </li>
+                            </ul>
+                        </li>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link  <?php if($page=='approve_comment'){echo 'active';}?>" href="ApproveComment.php">Approved Comments</a>
-                    </li><li class="nav-item">
-                        <a class="nav-link  <?php if($page=='spam_comment'){echo 'active';}?>" href="SpamComment.php">Spam Comments</a>
-                    </li>
+
                 </ul>
+
+                
                 <span class="navbar-text">
                     <a class="nav-link" type="submit" href="../controller/AdminController.php?fn=log_out&">Log out</a>
                 </span>
