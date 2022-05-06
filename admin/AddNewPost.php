5b708128c9e38392c1c02bceeffabfc58d4c8d69
yoelagustinus <yoelagustinus7@gmail.com>
2022-05-06 11:17:15 +0700

    Create AddNewPost.php

---
+++ admin/AddNewPost.php
@@ -0,0 +1,12 @@
+<?php
+    $page='list_content';
+    require 'header.php';
+    include '../controller/AdminController.php';
+
+?>
+
+<div class="container">
+    <h2 style="margin-top: 2rem;">Add New Post</h2>
+
+    
+</div>
